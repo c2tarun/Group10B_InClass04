32568f422f3350196562ed36e17deb48d238015b
Tarun K. Mall <c2tarun@hotmail.com>
2015-01-29 19:10:48 -0500

    init setup

---
+++ gen/com/example/group10b_inclass04/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.group10b_inclass04;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/example/group10b_inclass04/MainActivity.java
+++ src/com/example/group10b_inclass04/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Group10B_InClass04
@@ -15,10 +17,37 @@ import android.view.MenuItem;
  * MainActivity.java
  */
 public class MainActivity extends Activity {
+	
+	SeekBar complexityBar;
+	TextView complexityValue;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		complexityValue = (TextView) findViewById(R.id.complexityValue);
+		
+		complexityBar = (SeekBar) findViewById(R.id.complexityBar);
+		complexityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				complexityValue.setText(progress + "");
+			}
+		});
 	}
 }
