12dbaf260436ee9fe02e24a38ff3abe03e6171ea
Tarun K. Mall <c2tarun@hotmail.com>
2015-01-29 19:50:05 -0500

    Async working

--- gen/com/example/group10b_inclass04/R.java
+++ gen/com/example/group10b_inclass04/R.java
@@ -26,6 +26,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int button_async=0x7f040005;
+        public static final int button_thread=0x7f040004;
+        public static final int complexity_label_string=0x7f040003;
+        public static final int defaultComplexityValue=0x7f040002;
         public static final int hello_world=0x7f040001;
     }
     public static final class style {
--- src/com/example/group10b_inclass04/MainActivity.java
+++ src/com/example/group10b_inclass04/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.group10b_inclass04;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -17,37 +23,107 @@ import android.widget.TextView;
  * MainActivity.java
  */
 public class MainActivity extends Activity {
-	
+
 	SeekBar complexityBar;
 	TextView complexityValue;
+	TextView resultView;
+
+	ProgressDialog asyncProgress;
+	Button asyncButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		complexityValue = (TextView) findViewById(R.id.complexityValue);
-		
+		resultView = (TextView) findViewById(R.id.result);
+
 		complexityBar = (SeekBar) findViewById(R.id.complexityBar);
-		complexityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-			
+		complexityBar
+				.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+					@Override
+					public void onStopTrackingTouch(SeekBar seekBar) {
+
+					}
+
+					@Override
+					public void onStartTrackingTouch(SeekBar seekBar) {
+
+					}
+
+					@Override
+					public void onProgressChanged(SeekBar seekBar,
+							int progress, boolean fromUser) {
+						// TODO Auto-generated method stub
+						progress = (progress == 0) ? 1 : progress;
+						complexityValue.setText(progress + "");
+					}
+				});
+		asyncButton = (Button) findViewById(R.id.asyncButton);
+		asyncButton.setOnClickListener(new View.OnClickListener() {
+
 			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
+			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				// TODO Auto-generated method stub
-				complexityValue.setText(progress + "");
+				int complexity = complexityBar.getProgress();
+				complexity = (complexity == 0) ? 1 : complexity;
+				new DoWork().execute(complexity);
 			}
 		});
+
 	}
+
+	class DoWork extends AsyncTask<Integer, Integer, Double> {
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			asyncProgress = new ProgressDialog(MainActivity.this);
+			asyncProgress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			asyncProgress.setMax(100);
+			asyncProgress.setCancelable(false);
+			asyncProgress.show();
+		}
+
+		@Override
+		protected Double doInBackground(Integer... params) {
+			int counter = params[0];
+			Log.d("demo", "Counter: " + counter);
+
+			double number = 0.0d;
+
+			for (int i = 0; i < counter; i++) {
+				number += HeavyWork.getNumber();
+				publishProgress(calculateProgressValue(i + 1, counter));
+			}
+
+			number = number / counter;
+			return number;
+		}
+
+		public int calculateProgressValue(int i, int counter) {
+			Double percent = (double) ((i * 100) / counter);
+			return percent.intValue();
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+			Log.d("demo", "Progress Value: " + values[0]);
+			asyncProgress.setProgress(values[0]);
+		}
+
+		@Override
+		protected void onPostExecute(Double result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			asyncProgress.dismiss();
+			resultView.setText(result + "");
+		}
+
+	}
+
 }
